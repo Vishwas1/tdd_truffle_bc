2c30c66c2886dfbc6bc9da75bec78d6cfa55a27c
vishwasb <vishwasanand.cse@gmail.com>
2018-03-09 14:40:38 +0530

    Almost all TC implemented.

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -1,18 +1,25 @@
 pragma solidity ^0.4.4;
 contract Ballot {
   address public chairman;
+  uint partyCounter;
 
-  //Parties 
   struct Party {
     bool alreadyAdded;
     uint voteCount;
     bytes32 name;
   }
   mapping (address => Party)partListDict;
+
+  struct Voter {
+    bool hasVoted;
+    bool alreadyAdded;
+  }
+  mapping (address => Voter)public voterListDict;
+  
   bytes32 public ballotName;
 
   modifier onlyChairman() {
-    assert(chairman == msg.sender);
+    require(chairman == msg.sender);
     _;
   }
 
@@ -22,7 +29,9 @@ contract Ballot {
     ballotName = _ballotName;
   }
 
+
   /**
+     - Add party/proposal method
      - Party should not be duplicated
      - Only chairman can add party to the list.
    */
@@ -33,13 +42,40 @@ contract Ballot {
       partListDict[_partyAddr].alreadyAdded = true;
   }
 
-  // function getPartyList() public returns (bytes32[]){
-    
-  //   bytes32[] memory partyNamesArr =  new bytes32[](gblpartyListArr.length);
-  //       for(uint i = 0; i < gblpartyListArr.length; i++){
-  //          partyNamesArr[i] = gblpartyList[gblpartyListArr[i]].name;
-  //       }
-  //       return partyNamesArr;
-    
-  // }
+  /**
+     - Add voter method
+     - voter should not be duplicated
+     - Only chairman can add party to the list.
+   */
+  function addVoter(address _voterAddr) public onlyChairman {
+    require(_voterAddr != address(0) && !voterListDict[_voterAddr].alreadyAdded);
+      voterListDict[_voterAddr].hasVoted = false;
+      voterListDict[_voterAddr].alreadyAdded = true;
+      
+  }
+
+  modifier isValidVoter(){
+    require(voterListDict[msg.sender].alreadyAdded == true 
+    &&  voterListDict[msg.sender].hasVoted == false);
+    _;
+  }
+
+  /**
+  - doVoting -  will be called by voter. a party can also be a voter.
+  - voter should be present in the voterlist and should not have voted so far
+  - party should be present in the list
+  */
+  function doVoting(address _partyAddr) public isValidVoter {
+    require(_partyAddr != address(0) && partListDict[_partyAddr].alreadyAdded);
+    partListDict[_partyAddr].voteCount = partListDict[_partyAddr].voteCount + 1; 
+    voterListDict[msg.sender].hasVoted = true;
+  } 
+
+  /**
+    - getVoteCount -  fetch voter count wrt party
+   */
+  function getVoteCount(address _partyAddr) public view returns(uint) {
+    require(_partyAddr != address(0) && partListDict[_partyAddr].alreadyAdded);
+    return partListDict[_partyAddr].voteCount;
+  }
 }
--- test/ballot.js
+++ test/ballot.js
@@ -1,36 +1,129 @@
+/**Test cases for Ballot contract.*/
+'use strict';
 const Ballot =  artifacts.require("./Ballot.sol");
 
 contract('Ballot', function(accounts) {
   let BallotInst;
-  let chairman = accounts[0];
+  let chairman = accounts[0]; //0x627306090abab3a6e1400e9345bc60c78a8bef57
   let ballotname = 'Test Ballot';
-  let partyAddrs = ["0xf17f52151ebef6c7334fad080c5704d77216b732", "0xc5fdf4076b8f3a5357c5e395ab970b5b54098fef"];
+  let partyAddrs = [accounts[1],accounts[2]];
   let partyNames = ["Party01", "Party02"];
+  let voterAddrs = [accounts[1],accounts[2],accounts[3], accounts[4], accounts[5], accounts[6], 
+  accounts[7], accounts[8]];
   
-  before("getting instance before all Test Cases", async function(){
+  
+  before('getting instance before all Test Cases', async function(){
     console.log(ballotname);
     BallotInst = await Ballot.new(ballotname);
+  })
+
+  describe('Deploying contract', function () {
+    it('should test that the smart contract is deployed', async function () {
+        assert.equal(web3.isAddress(BallotInst.address), true)
+    })
+  })
+
+  describe('Checking constructor parameters', function(){
+    it('should have chairman', async function(){
+      assert.equal(await BallotInst.chairman(), chairman, 'Test Failed. chairman is not assigned.');
+    });
+  
+    it('should have ballot name', async function(){
+      let result = await BallotInst.ballotName();
+      assert.equal(web3.toAscii(result.replace(/0+$/,'')), ballotname, 'Test Failed. ballotname is not assigned.');
+    })
+  })
+
+  describe('Adding party to the list', function(){
+    it('chairman should be able to add party only', async function(){
+      try{
+        //adding party from some other account
+        await BallotInst.addParty(partyAddrs[0], partyNames[0], {
+          from: accounts[1]
+        });
+        assert.fail();
+      }catch(err){
+        //console.log(err.toString());
+      }
+    })
+
+    it('no duplicate party should be added',async function(){
+      try{
+        //adding party 0 
+        await BallotInst.addParty(partyAddrs[0], partyNames[0]);
+        await BallotInst.addParty(partyAddrs[1], partyNames[1]);
+        //trying to add same party again
+        await BallotInst.addParty(partyAddrs[0], partyNames[0]);
+        //test should fail
+        assert.fail();
+      }catch(err){
+        //console.log(err.toString());
+      }
+    })
   });
 
-  it('chairman is assigned', async function(){
-    assert.equal(await BallotInst.chairman(), chairman, 'Test Failed. chairman is not assigned.');
+  describe('Adding voter to the list and check validity', function(){
+    it('chairman should be able to add voter only', async function(){
+      try{
+        //adding party from some other account
+        await BallotInst.addVoter(voterAddrs[0], {
+          from: accounts[1]
+        });
+        assert.fail();
+      }catch(err){
+        //console.log(err.toString());
+      }
+    })
+
+    it('no duplicate voter should be added',async function(){
+      try{
+        //adding voter 0,1,2,3
+        await BallotInst.addVoter(voterAddrs[0]);
+        await BallotInst.addVoter(voterAddrs[1]);
+        await BallotInst.addVoter(voterAddrs[2]);
+        await BallotInst.addVoter(voterAddrs[3]);
+        await BallotInst.addVoter(voterAddrs[4]);
+        await BallotInst.addVoter(voterAddrs[5]);
+
+        //adding voter 0 again
+        await BallotInst.addVoter(voterAddrs[0]);
+        //test should fail
+        assert.fail();
+      }catch(err){
+        //console.log(err.toString());
+      }
+    })
   });
 
-  it('has a ballot name', async function(){
-    let result = await BallotInst.ballotName();
-    assert.equal(web3.toAscii(result), ballotname, 'Test Failed. ballotname is not assigned.');
-  });
+  describe('Do voting',function(){
+    it('vote count should increase', async function(){
+      let prevVoteCnt = await BallotInst.getVoteCount(partyAddrs[0])
+      //console.log('prev voter cnt = ' + prevVoteCnt);
+      await BallotInst.doVoting(partyAddrs[0], {from : voterAddrs[0]})
+      await BallotInst.doVoting(partyAddrs[0], {from : voterAddrs[1]})
+      await BallotInst.doVoting(partyAddrs[0], {from : voterAddrs[2]})
+      let aftVoteCnt = await BallotInst.getVoteCount(partyAddrs[0])
+      //console.log('after voter cnt = ' + aftVoteCnt);
+      assert.isAbove(aftVoteCnt, prevVoteCnt, 'Test failed')
+    })
 
-  it('only chairman should be able to add party', async function(){
-    try{
-      await BallotInst.addParty(partyAddrs[0], partyNames[0], {
-        from: accounts[1]
-      });
-      assert.fail();
-    }catch (error) {
-      //console.log(error);
-      assert(error.toString().includes('invalid opcode'), error.toString())
-    }
+    it('should fail since voter 0 already voted', async function(){
+      try{
+        await BallotInst.doVoting(partyAddrs[0], {from : voterAddrs[0]})
+        assert.fail();
+      }catch(err){}
+    })
+  })
+
+  describe('Getting vote counts for a party', function(){
+    it('should return 6 vote count for party 0', async function(){
+      //await BallotInst.doVoting(partyAddrs[0], {from : voterAddrs[2]})
+      await BallotInst.doVoting(partyAddrs[0], {from : voterAddrs[3]})
+      await BallotInst.doVoting(partyAddrs[0], {from : voterAddrs[4]})
+      await BallotInst.doVoting(partyAddrs[0], {from : voterAddrs[5]})
+      let result = await BallotInst.getVoteCount(partyAddrs[0]);
+      assert.equal(result.valueOf(), 6, 'Test Failed');
+    })
   })
 });
 
